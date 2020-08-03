1a97ef9578170781b220d0a2f2ec8cbeb43d98a9
Ta Gr <tatevikg1@gmail.com>
2020-08-03 14:00:45 +0400

    middleware that allows the user pass test only once

---
+++ app/Http/Middleware/SecondTime.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class SecondTime
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        $topic_id = $request->route('topic')->id;
+
+        $topics = DB::table('tests')->where('user_id', $user->id)->pluck('topic_id');
+        $topics = $topics->toArray();
+
+
+        if (in_array($topic_id, $topics)) {
+
+            return  redirect('/secondTime');
+        }
+        return $next($request);
+    }
+}
