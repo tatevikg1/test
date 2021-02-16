fe9fbeb19715b2461d7b6251e11e52bac3419526
ta <tatevikg1@gmail.com>
2021-02-16 19:44:25 +0400

    style in div for heroku

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -7,7 +7,8 @@
     <div class="row"> 
         @foreach ($topics as $topic)
 
-            <div class="m-4 p-4 topic" style="width:200px">
+            <div class="m-4 p-4 topic" 
+                 style="width:200px;background-color: #f7f7f7; border: 1px solid grey; border-radius: 10px;">
 
                 <p>{{ ucfirst($topic->title) }}</p>
                 <a href="/tests/{{ $topic->id }}-{{Str::slug($topic->title)}}" class="btn btn-dark"> 
